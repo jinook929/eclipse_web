d6fb8f6b10e84ce792739dddacd7d48137586743
jinook929 <jinook929@gmail.com>
2020-01-04 20:56:22 -0500

    initial commit

---
+++ src/controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet.web")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String userId = (String)request.getParameter("USERID");
+		String userPw = (String)request.getParameter("USERPW");
+		
+		System.out.println(userId);
+		System.out.println(userPw);
+		
+		if(userId.equals("Jinook") && userPw.equals("1111")) {
+			response.sendRedirect("loginSuccess.html");
+		} else {
+			response.sendRedirect("loginFailure.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
